46c1f51767552f0d8b00e62a79f01d725e153b1f
Marcelo Junior <marcelo.junior@enjoei.com.br>
2022-07-14 22:48:55 -0300

    Add function Rom.NextByte

---
+++ chip8.go
@@ -0,0 +1,24 @@
+package chip8
+
+import (
+	"io"
+)
+
+type Instruction []byte
+
+type Rom struct {
+	data io.Reader
+}
+
+// NewRom is a function that receive a "data" as param and return a pointer to Rom
+func NewRom(data io.Reader) *Rom {
+	return &Rom{data: data}
+}
+
+// NextInstruction return the next instruction
+func (r *Rom) NextByte() *Instruction {
+	instr := make(Instruction, 2)
+	r.data.Read(instr)
+
+	return &instr
+}
---
+++ tests/rom_test.go
@@ -0,0 +1,35 @@
+package rom_test
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	chip8 "github.com/MarceloMPJR/go-chip-8"
+)
+
+func TestRom_NextInstruction(t *testing.T) {
+	buf := chip8.Instruction("MARCEL")
+
+	data := bytes.NewBuffer(buf)
+
+	rom := chip8.NewRom(data)
+
+	for i := 0; i < len(buf); i += 2 {
+		expected := chip8.Instruction(buf[i : i+2])
+
+		checkInstruction(t, rom.NextByte(), &expected)
+	}
+}
+
+func checkInstruction(t *testing.T, result, expected *chip8.Instruction) {
+	t.Helper()
+
+	if result == nil {
+		t.Fatal("result is nil")
+	}
+
+	if !reflect.DeepEqual(*result, *expected) {
+		t.Errorf("result %v, expected %v", *result, *expected)
+	}
+}
